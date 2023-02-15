6e64d473a35fd4774b0668bfbe5e4672f7f0b41c
Claudinei de Jesus Braine <50373192+claubraine@users.noreply.github.com>
2023-02-15 11:16:49 -0300

    15-02-2023

---
+++ pages/api/generate.js
@@ -0,0 +1,57 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const question = req.body.question || '';
+  if (question.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid question",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Classify the sentiment in these tweets:\n1. \"I can't stand homework\"\n2. " + question,
+      temperature: 0,
+      max_tokens: 60,
+      top_p:1.0,
+      frequency_penalty:0.0,
+      presence_penalty:0.0
+    });
+    //console.log(completion.data)
+    const text = completion.data.choices[0].text
+    const arrayText = text.split("2. ")
+    console.log(arrayText[1])
+    res.status(200).json({ result: arrayText[1] });
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
